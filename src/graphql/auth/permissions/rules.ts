2999f98bf995a31e2f2c9e3774087e2b0b94a40d
Ralf <ralflopez3@gmail.com>
2022-03-05 15:37:57 +0800

    :wheelchair: add graphql shield error messages

--- src/graphql/auth/permissions/rules.ts
+++ src/graphql/auth/permissions/rules.ts
@@ -1,15 +1,20 @@
 import { and, rule } from "graphql-shield"
 import { Context } from "../../../config/context"
 import { getUserId } from "../../../vendor/victoriris/authUtil"
+import {
+  AuthenticationError,
+  ForbiddenError,
+  UserInputError,
+} from "apollo-server-errors"
 
 export const isAuthenticated = rule({
   /*cache: "contextual" */
 })(async (_parent, _args, context: Context) => {
   const userId = getUserId({ request: context.request })
-  if (!userId) return false
+  if (!userId) throw new AuthenticationError("No token provided")
 
   const exists = await context.dataSources.user.getOne(userId)
-  if (!exists) return false
+  if (!exists) throw new UserInputError("User doesn't exist")
 
   context.user = { id: userId as string }
   return true
@@ -17,11 +22,14 @@ export const isAuthenticated = rule({
 
 const selfRule = rule({})(async (_parent, _args, context: Context) => {
   const userId = getUserId({ request: context.request })
-  if (!userId) return false
+  if (!userId) throw new AuthenticationError("No token provided")
 
   const storedUser = await context.dataSources.user.getOne(userId)
 
-  return storedUser.id == userId
+  if (storedUser.id !== userId)
+    throw new ForbiddenError("You don't own this user")
+
+  return true
 })
 
 export const isSelf = and(isAuthenticated, selfRule)
@@ -29,13 +37,16 @@ export const isSelf = and(isAuthenticated, selfRule)
 const adminRule = rule({
   /* cache: "contextual" */
 })(async (_parent, _args, context: Context) => {
+  console.log("OK")
   const id = context.user?.id
-  if (!id) return false
+  if (!id) throw new AuthenticationError("No token provided")
 
   const user = await context.dataSources.user.getOne(id)
-  if (!user) return false
+  if (!user) throw new UserInputError("User not found")
 
-  return user?.role === "ADMIN"
+  if (user?.role !== "ADMIN") throw new ForbiddenError("You are not an admin")
+
+  return true
 })
 
 export const isAdmin = and(isAuthenticated, adminRule)
