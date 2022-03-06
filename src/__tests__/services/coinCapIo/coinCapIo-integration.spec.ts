8b853c12d7eb9ffd70248fada871ba5a50db8b38
Ralf <ralflopez3@gmail.com>
2022-03-06 23:57:22 +0800

    :white_check_mark: basic coin cap io dataSource integration test

---
+++ src/__tests__/services/coinCapIo/coinCapIo-integration.spec.ts
@@ -0,0 +1,44 @@
+import { CoinCapIoDataSource } from "../../../services/coinCapIo/coinCapIoDataSource"
+
+let coinCapIoDataSource: CoinCapIoDataSource
+
+beforeAll(() => {
+  coinCapIoDataSource = new CoinCapIoDataSource()
+})
+
+describe("getAllAssets", () => {
+  it("should return details", async () => {
+    const result = await coinCapIoDataSource.getAllAssets()
+    expect(result).toBeDefined()
+    expect(Array.isArray(result)).toBeTruthy()
+    expect(result).toEqual(
+      expect.arrayContaining([expect.objectContaining({ symbol: "BTC" })])
+    )
+  })
+})
+
+describe("getAsset", () => {
+  it("should return details", async () => {
+    const result = await coinCapIoDataSource.getAsset("bitcoin")
+    expect(result).toBeDefined()
+    expect(result).toEqual(expect.objectContaining({ symbol: "BTC" }))
+  })
+})
+
+describe("getRates", () => {
+  it("should return details", async () => {
+    const result = await coinCapIoDataSource.getRates()
+    expect(result).toBeDefined()
+    expect(result).toEqual(
+      expect.arrayContaining([expect.objectContaining({ symbol: "PHP" })])
+    )
+  })
+})
+
+describe("getRate", () => {
+  it("should return details", async () => {
+    const result = await coinCapIoDataSource.getRate("philippine-peso")
+    expect(result).toBeDefined()
+    expect(result).toEqual(expect.objectContaining({ symbol: "PHP" }))
+  })
+})
