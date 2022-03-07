3182f64fffe9bc8de5feb79a26dcef241b9a4f92
Ralf <ralflopez3@gmail.com>
2022-03-07 13:22:00 +0800

    :recycle: use a class for redis client for easy depencency injection

--- src/config/redis/client.ts
+++ src/config/redis/client.ts
@@ -1,13 +1,45 @@
-import { RedisClientType } from "@node-redis/client"
-import { createClient } from "redis"
-export const client: RedisClientType = createClient({
-  url: "redis://localhost:6379",
-})
-client.on("error", (err) => console.log("Redis Client Error", err))
+import { createClient, RedisClientType } from "redis"
+import dotenv from "dotenv"
+dotenv.config()
 
-export const withRedisClient = async (
-  callback: (client: RedisClientType) => Promise<any>
-) => {
-  await client.connect()
-  return callback(client).then(() => client.quit())
+export class RedisClient {
+  client: RedisClientType
+  constructor() {
+    this.client = createClient({
+      url: process.env.REDIS_URL || "redis://localhost:6379",
+    })
+
+    this.client.on("error", (err) => {
+      console.log("Redis Client Error", err)
+    })
+  }
+
+  quit() {
+    this.client.quit()
+  }
+
+  async checkCache<ReturnType>(key: string): Promise<ReturnType | null> {
+    let parsedValue: ReturnType | null = null
+
+    try {
+      await this.client.connect()
+      const cacheValue = await this.client.get(key)
+      if (cacheValue) parsedValue = JSON.parse(cacheValue) as ReturnType
+    } finally {
+      await this.client.quit()
+    }
+
+    return parsedValue
+  }
+
+  async storeCache(key: string, value: string) {
+    try {
+      await this.client.connect()
+      await this.client.set(key, value)
+    } finally {
+      await this.client.quit()
+    }
+  }
 }
+
+export const redisClient = new RedisClient()
