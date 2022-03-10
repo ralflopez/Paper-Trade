7d1eee47c4aed20faccb571324bc1d6a66406885
Ralf <ralflopez3@gmail.com>
2022-03-10 17:12:23 +0800

    :white_check_mark: add auth module test

--- src/graphql/auth/authResolver.ts
+++ src/graphql/auth/authResolver.ts
@@ -6,8 +6,8 @@ import {
   getRefreshCookie,
   removeRefreshCookie,
 } from "../../vendor/victoriris/authUtil"
-import { AuthenticationError } from "apollo-server-errors"
-
+import { AuthenticationError, UserInputError } from "apollo-server-errors"
+import { isValidEmail } from "../../helpers/auth/isValidEmail"
 // Query
 export const logoutQuery = queryField("logout", {
   type: "Boolean",
@@ -17,7 +17,7 @@ export const logoutQuery = queryField("logout", {
   },
 })
 
-export const RefreshTokenQuery = queryField("refreshToken", {
+export const refreshTokenQuery = queryField("refreshToken", {
   type: "AuthPayload",
   resolve: async (_parent, _args, context): Promise<AuthPayload> => {
     const refreshToken = getRefreshCookie({ request: context.request })
@@ -43,11 +43,22 @@ export const signupMutation = mutationField("signup", {
   args: {
     data: nonNull("SignupInput"),
   },
-  resolve: async (_parent, args, context): Promise<AuthPayload> => {
+  resolve: async (
+    _parent,
+    { data: { email, password, name } },
+    context
+  ): Promise<AuthPayload> => {
+    // validation
+    if (!isValidEmail(email))
+      throw new UserInputError(`Invalid Email: ${email}`)
+    if (password.length < 8)
+      throw new UserInputError("Password must be atleast 8 characters")
+    if (name.length < 1) throw new UserInputError("Name cannot be blank")
+
     const user: User = await context.dataSources.user.createOne(
-      args.data.email,
-      args.data.password,
-      args.data.name
+      email,
+      password,
+      name
     )
 
     const { accessToken } = await createTokens({ userId: user.id }, context)
@@ -66,10 +77,14 @@ export const loginMutation = mutationField("login", {
   args: {
     data: nonNull("LoginInput"),
   },
-  resolve: async (_parent, args, context): Promise<AuthPayload> => {
+  resolve: async (
+    _parent,
+    { data: { email, password } },
+    context
+  ): Promise<AuthPayload> => {
     const user: User = await context.dataSources.user.getOneByEmailAndPassword(
-      args.data.email,
-      args.data.password
+      email,
+      password
     )
 
     const { accessToken } = await createTokens({ userId: user.id }, context)
