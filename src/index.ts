f5ea6d94c9d694ca2909597912ab6eb90002b851
Ralf <ralflopez3@gmail.com>
2022-06-09 15:01:37 +0800

    :bricks: use PORT env variable from heroku

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ async function startApolloServer(
   app: Express
 ) {
   await new Promise<void>((resolve) =>
-    httpServer.listen({ port: 4000 }, resolve)
+    httpServer.listen({ port: process.env.PORT || 4000 }, resolve)
   )
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 }
